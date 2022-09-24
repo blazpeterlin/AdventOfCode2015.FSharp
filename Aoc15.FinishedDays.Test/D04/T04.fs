11c658f96347a93fd611b1dffbfb67e09fc40a24
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-09-24 23:44:25 +0200

    D04

---
+++ Aoc15.FinishedDays.Test/D04/T04.fs
@@ -0,0 +1,26 @@
+module Aoc15.ActiveDay.T04
+
+open Aoc15.Input
+open Aoc15.D04
+open NUnit.Framework
+open System
+
+[<SetUp>]
+let Setup () =
+    ()
+
+let basePath = System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name.Replace("T","D")
+let prependFolder fname = basePath + "\\" + fname
+    
+[<TestCase("input.txt", 346386)>] 
+[<TestCase("input-TEST.txt", 609043)>] 
+let Test1 (fn : string, res: int) = 
+    let input = fn |> prependFolder |> f2text
+    let sln1 = solve1 input
+    Assert.That(sln1, Is.EqualTo res)
+    
+[<TestCase("input.txt", 9958218)>] 
+let Test2 (fn : string, res: int) = 
+    let input = fn |> prependFolder |> f2text
+    let sln2 = solve2 input
+    Assert.That(sln2, Is.EqualTo res)
---
+++ Aoc15.FinishedDays/D04.fs
@@ -0,0 +1,46 @@
+﻿module Aoc15.D04
+
+open Aoc15
+open Input
+open TextCopy
+open System.Security.Cryptography
+open System.Text
+
+let input =  "input.txt" |> f2text
+
+let MD5Hash (input : string) =
+      use md5 = System.Security.Cryptography.MD5.Create()
+      input
+      |> System.Text.Encoding.ASCII.GetBytes
+      |> md5.ComputeHash
+      |> Seq.map (fun c -> c.ToString("X2"))
+      |> Seq.reduce (+)
+
+let parseLine (ln:string) =
+    ln
+    |> text2tokens "x"
+    //|> List.map int
+    //|> fun (x::y::z::[]) -> (x,y,z)
+
+let parse2lines (text:string) = 
+    text
+    |> text2lines 
+    |> List.map parseLine
+
+let solve1 (text:string) = 
+    Seq.initInfinite id
+    |> Seq.filter (fun i -> MD5Hash (text+i.ToString()) |> fun x -> x.StartsWith "00000")
+    |> Seq.head
+    
+let solve2 (text:string) =
+    Seq.initInfinite id
+    |> Seq.filter (fun i -> MD5Hash (text+i.ToString()) |> fun x -> x.StartsWith "000000")
+    |> Seq.head
+
+let res1 = input |> solve1
+let res2 = input |> solve2
+
+ClipboardService.SetText(res2.ToString())
+
+let finished = true
+()
